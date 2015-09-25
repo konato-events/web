809380aca50730b7c13bcd2d0dd5123d228410a6
Igor Santos <igorsantos07@gmail.com>
2015-09-25 00:01:40 -0300

    assets/
    Removing Semantic UI. refs #28

--- resources/views/layout-master.blade.php
+++ resources/views/layout-master.blade.php
@@ -250,18 +250,6 @@
 
                 <div class="row thumbnails no-padding gallery">
                     <?php
-                        $events = [
-                            1 => ['/img/event-sample1.jpg', 'iMasters Developer Week RJ' , 'Rio de Janeiro, Brazil'],
-                            2 => ['/img/event-sample2.jpg', 'PHP\'n Rio 2011', 'Rio de Janeiro, Brazil'],
-                            3 => ['/img/event-sample3.jpg', 'PHPConf 2015', 'Osasco, Brazil'],
-                            4 => ['/img/event-sample4.jpg', 'TDCOnline 2015 POA', 'Porto Alegre, Brazil'],
-                            5 => ['/img/event-sample5.jpg', 'O\'Reilly\'s Fluent', 'San Francisco, USA'],
-                            6 => ['/img/event-sample6.gif', 'UERJ Sem Muros', 'Rio de Janeiro, Brazil'],
-                            7 => ['/img/event-sample7.jpg', '53º Congresso HUPE', 'Rio de Janeiro, Brazil'],
-                            8 => ['/img/event-sample8.png', 'XXVI Congresso Brasileiro de Virologia', 'Florianópolis,
-                            Brazil']
-                        ];
-                        shuffle($events);
                         foreach($events as $event):
                             list($img, $title, $desc) = $event;
                             $dimensions = getimagesize(APP_ROOT.'/public'.$img);
