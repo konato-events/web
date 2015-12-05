a19b81892c66514d800e8fc5b78b4e03f0893fe6
Igor Santos <igorsantos07@gmail.com>
2015-12-05 01:00:17 -0200

    Displaying event themes in the details. refs #19, #14

--- resources/views/components/themes_list.blade.php
+++ resources/views/components/themes_list.blade.php
@@ -1,7 +1,7 @@
 <?php
-/** @var array  $themes */
-/** @var bool   $speaker */
-/** @var string $gender */
+/** @var Theme[] $themes */
+/** @var bool    $speaker */
+/** @var string  $gender */
 
 $speaker = $speaker ?? false;
 $gender  = $gender ?? 'M';
@@ -9,10 +9,10 @@ $route = $speaker? 'speaker@theme' : 'event@theme';
     $tooltip = _r('How many times %s has spoken about this subject', ($gender == 'M')? _('he') : _('she'));
 ?>
 <ul class="themes">
-    <?php foreach($themes as $id => $name): ?>
+    <?php foreach($themes as $theme): ?>
         <li<?=($speaker)? ' class="composed"':''?>>
-            <a href="<?=act($route, slugify($id, $name))?>">
-                {{$name}}
+            <a href="<?=act($route, $theme->slug)?>">
+                {{$theme->name}}
                 <?php if($speaker): ?>
                     <span title="{{$tooltip}}" data-toggle="tooltip">{{rand(1,20)}}</span>
                 <?php endif ?>
--- resources/views/event/details.blade.php
+++ resources/views/event/details.blade.php
@@ -401,17 +401,15 @@ $dates_str = function(bool $compact = false) use ($event, $date_fmt):string {
                     </div>
                 </div>
 
-                <!--
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4 class="panel-title"><?=_('Main themes')?></h4>
                     </div>
                     <div class="panel-body">
                         <? //TODO: display here even themes that there was no talk on it; sort by number of talks given ?>
-                        @include('components.themes_list')
+                        @include('components.themes_list', ['themes' => $event->themes])
                     </div>
                 </div>
-                -->
 
                 <!--
                 <div class="panel panel-default">
