d10827788b6ac0c065b7b5f8318117775a8bbf34
Igor Santos <igorsantos07@gmail.com>
2015-12-05 00:11:42 -0200

    Fixing autocomplete controller and including ability to filter. refs #75, #77

--- app/Http/Controllers/AutoCompleteController.php
+++ app/Http/Controllers/AutoCompleteController.php
@@ -6,28 +6,32 @@ class AutoCompleteController extends Controller {
 
     public function missingMethod($params = []) {
         $available_objects = [
-            'themes' => 'Theme'
+            'themes' => 'Theme',
+            'users'  => 'User',
         ];
 
         if (!isset($params[0])) {
             //TODO: verify the correct HTTP code
-            return response('Missing object type ['.join(' | ',array_keys($available_objects)).'', 400);
+            return response('Missing object type ['.join(' | ',array_keys($available_objects)).']', 400);
         } else {
             if (array_key_exists($params[0], $available_objects)) {
-
+                $class = '\App\Models\\'.$available_objects[$params[0]];
             } else {
-                return response('Wrong object type ['.join(' | ',array_keys($available_objects)).'', 400);
+                return response('Wrong object type ['.join(' | ',array_keys($available_objects)).']', 400);
             }
         }
 
-        $class = '\App\Models\\';
-        switch ($params[0]) {
-            case 'themes': $class .= 'Theme'; break;
-            default:
+        if (isset($_GET['q'])) {
+            $data = call_user_func([$class, 'where'], 'name', 'ILIKE', "%{$_GET['q']}%")->get();
+        } else {
+            $data = call_user_func([$class, 'all']);
         }
 
         return JsonResponse::create(
-            call_user_func([$class, 'all'])->pluck('name', 'id')
+            array_map(
+                function($obj) { return [ 'id' => $obj->id, 'text' => $obj->name, 'slug' => $obj->name ]; },
+                $data->all()
+            )
         );
     }
 
