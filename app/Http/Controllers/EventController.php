816214d5e3058ea0e324496c8cb3067b8ff98122
Igor Santos <igorsantos07@gmail.com>
2016-03-13 23:54:04 -0300

    Correctly ordering search results (by date)

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -45,7 +45,7 @@ class EventController extends Controller {
         if ($paid)  { $db_query->where('free', ($paid == -1)); }
         if ($place) { $db_query->where('location', 'ilike', $ilikey($place)); }
         if ($types) { $db_query->whereIn('event_type_id', $types); }
-        $events = $db_query->get();
+        $events = $db_query->orderBy('begin', 'desc')->get();
 
         if (sizeof($events) == 1 && $query) {
             return redirect()->to(act('event@details', $events[0]->slug));
