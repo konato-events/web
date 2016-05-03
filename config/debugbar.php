0c45d4d1bfea8a4502e0d2363328c670a1063258
Igor Santos <igorsantos07@gmail.com>
2016-05-03 04:45:57 -0300

    Better debugbar configs

--- config/debugbar.php
+++ config/debugbar.php
@@ -83,11 +83,11 @@ return array(
 
     'collectors' => array(
         'phpinfo'         => true,  // Php version
-        'messages'        => true,  // Messages
+        'messages'        => false,  // Messages
         'time'            => true,  // Time Datalogger
         'memory'          => true,  // Memory usage
         'exceptions'      => true,  // Exception displayer
-        'log'             => true,  // Logs from Monolog (merged in messages if enabled)
+        'log'             => false,  // Logs from Monolog (merged in messages if enabled)
         'db'              => true,  // Show database (PDO) queries and bindings
         'views'           => true,  // Views with their data
         'route'           => true,  // Current route information
@@ -96,10 +96,10 @@ return array(
         'default_request' => false, // Regular or special Symfony request logger
         'symfony_request' => true,  // Only one can be enabled..
         'mail'            => true,  // Catch mail messages
-        'logs'            => false, // Add the latest log messages
+        'logs'            => true, // Add the latest log messages
         'files'           => false, // Show the included files
         'config'          => false, // Display config settings
-        'auth'            => false, // Display Laravel authentication status
+        'auth'            => true, // Display Laravel authentication status
         'session'         => true,  // Display session data
     ),
 
@@ -114,17 +114,17 @@ return array(
 
     'options' => array(
         'auth' => array(
-            'show_name' => false,   // Also show the users name/email in the debugbar
+            'show_name' => true,   // Also show the users name/email in the debugbar
         ),
         'db' => array(
             'with_params'       => true,   // Render SQL with the parameters substituted
-            'timeline'          => false,  // Add the queries to the timeline
+            'timeline'          => true,   // Add the queries to the timeline
             'backtrace'         => false,  // EXPERIMENTAL: Use a backtrace to find the origin of the query in your files.
             'explain' => array(            // EXPERIMENTAL: Show EXPLAIN output on queries
                 'enabled' => false,
                 'types' => array('SELECT'), // array('SELECT', 'INSERT', 'UPDATE', 'DELETE'); for MySQL 5.6.3+
             ),
-            'hints'             => true,    // Show hints for common mistakes
+            'hints'             => false,   // Show hints for common mistakes
         ),
         'mail' => array(
             'full_log' => false
@@ -133,7 +133,7 @@ return array(
             'data' => false,    //Note: Can slow down the application, because the data can be quite large..
         ),
         'route' => array(
-            'label' => true  // show complete route on bar
+            'label' => false  // show complete route on bar
         ),
         'logs' => array(
             'file' => null
